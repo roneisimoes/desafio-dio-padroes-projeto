91efae995465e5810fb921a042aa1c9b17983992
Ronei Simões <ronei.simoes19@gmail.com>
2023-02-17 16:16:34 -0300

    Initial commit

---
+++ src/main/java/one/digitalinnovation/gof/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,19 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Projeto guiado por falvojr
+ */
+
+@EnableFeignClients
+@SpringBootApplication
+public class LabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+	}
+
+}
